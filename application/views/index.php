6f673bf346bb6b0340ff8d31379ba95da2e17cf8
root <datlequoc96@gmail.com>
2017-02-06 21:39:10 +0700

    Delte unused part

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,6 @@
 <p>index page</p>
 <a class="navbar-brand" href="<?php echo base_url().'user/signup'; ?>">signup</a>
 <a class="navbar-brand" href="<?php echo base_url().'user/login'; ?>">login</a>
-<a class="navbar-brand"><?php echo base_url().'user/profile/'.$user_name; ?></a>
 <?php
 if($is_logged_in == 1){
 ?>
@@ -11,8 +10,8 @@ if($is_logged_in == 1){
 	}
 ?>
 
-<form action="<?php echo base_url() . "search/search_result"?>" method="post"> 
+<!-- <form action="<?php echo base_url() . "search/search_result"?>" method="post"> 
 Search: <input type="text" name="search_phrase" /><br /> 
 <input type="submit" value="Submit" /> 
 </form> 
-
+ -->
