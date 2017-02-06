4d81e0293c9d90818c951ff32523c6e860deb0dd
An Ngoc <ngocan.4595@gmail.com>
2017-02-06 13:40:44 +0700

    add module site check for security

---
+++ application/modules/site_security/controllers/site_security.php
@@ -0,0 +1,23 @@
+<?php
+class Site_security extends MX_Controller 
+{
+
+    function __construct() {
+    parent::__construct();
+    }
+
+    function _make_sure_is_admin()
+    {
+        $is_admin = false;
+
+        if ($is_admin != true) {
+            redirect('site_security/not_allowed');
+        }
+    }
+
+    function not_allowed()
+    {
+        echo 'Fuck u bitch';
+    }
+
+}
