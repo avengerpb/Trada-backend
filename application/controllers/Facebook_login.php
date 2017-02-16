a7fb0746b0f2ad420f671a1c91b6a5d4d8f3e3fb
baodoanusth <baodoan1131996@gmail.com>
2017-02-16 11:23:47 +0700

    can login with facebook account

---
+++ application/controllers/Facebook_login.php
@@ -0,0 +1,68 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once __DIR__ . '\php-graph-sdk-5.4\src\Facebook\autoload.php';
+
+class Facebook_login extends CI_Controller {
+
+	public function __construct() {
+		
+		parent::__construct();
+		$this->load->library(array('session'));
+		$this->load->helper(array('url'));
+		$this->load->model('model_users');
+		
+		$fb = new Facebook\Facebook([
+  			'app_id' => '1175112432606251',
+  			'app_secret' => 'f26d7b0706b3c2f1a87fe30f0be28f17',
+  			'default_graph_version' => 'v2.5',
+		]);	
+	}
+
+	public function fb_login(){
+		
+		$fb = new Facebook\Facebook([
+  			'app_id' => '1175112432606251',
+  			'app_secret' => 'f26d7b0706b3c2f1a87fe30f0be28f17',
+  			'default_graph_version' => 'v2.5',
+		]);
+
+		$helper = $fb->getRedirectLoginHelper();
+		$permissions = ['public_profile', 'user_friends', 'publish_actions']; // optional
+		$data['login_url'] = $helper->getLoginUrl('http://localhost/trada-backend/index.php/facebook_login/fb_callback', $permissions);
+
+		$this->load->view('home',$data);
+	}
+
+
+	public function fb_callback(){
+		$fb = new Facebook\Facebook([
+			'app_id' => '1175112432606251',
+  			'app_secret' => 'f26d7b0706b3c2f1a87fe30f0be28f17',
+  			'default_graph_version' => 'v2.5',
+  		]);
+
+		$helper = $fb->getRedirectLoginHelper();
+		try {
+  			$accessToken = $helper->getAccessToken();
+		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  			// When Graph returns an error
+  			echo 'Graph returned an error: ' . $e->getMessage();
+  			exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  			// When validation fails or other local issues
+  			echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  			exit;
+			}
+
+		if (isset($accessToken)) {
+  			// Logged in!
+  			$_SESSION['facebook_access_token'] = (string) $accessToken;
+  			redirect(base_url()."user/index");
+  			$
+  			// Now you can redirect to another page and use the
+  			// access token from $_SESSION['facebook_access_token']
+		}
+	}
+}
+?>
---
+++ application/views/home.php
@@ -0,0 +1,5 @@
+<?php
+include_once "index.php";
+?>
+
+<a href="<?= $login_url ?>">Login</a>
