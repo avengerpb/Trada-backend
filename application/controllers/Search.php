d48b623637a68c4db53fe8bcb1f6cf4d800ee123
Nguyen Minh Duc <nguyenminhduc1803@gmail.com>
2017-02-25 04:01:19 +0700

    handle empty input from search bar

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -6,14 +6,16 @@ class Search extends CI_Controller {
 		$this->load->model('Model_search');
 		/*$search_phrase =  $this->input->post('search_phrase');*/
 		$search_phrase = $_POST['search_phrase'];
-		$result['item'] = $this->Model_search->search_item($search_phrase);
-		$result['shop'] = $this->Model_search->search_shop($search_phrase);
-		$result['user'] = $this->Model_search->search_user($search_phrase);
-		/*$this->load->view('search_result',$result);*/
-		echo json_encode($result);
-		/*$data = json_encode($result);
-		setcookie('search', $data, time()+1, "/");
-		include('http://localhost/trada-frontend/search_result.html');
-		redirect('http://localhost/trada-frontend/search_result.html');*/
+		if($search_phrase == ''){
+			$search_phrase = array();
+			echo json_encode($search_phrase);
+		}
+		else{
+			$result['item'] = $this->Model_search->search_item($search_phrase);
+			$result['shop'] = $this->Model_search->search_shop($search_phrase);
+			$result['user'] = $this->Model_search->search_user($search_phrase);
+			/*$this->load->view('search_result',$result);*/
+			echo json_encode($result);
+		}
 	}
 }
